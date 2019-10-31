6d60c11c0941e62021a69e6debdee765b66bd81b
Ethan Miller <Ethan.Miller@improving.com>
2019-10-31 14:14:23 -0500

    initial commit

---
+++ src/main/java/org/improving/WordRetriever.java
@@ -0,0 +1,16 @@
+package org.improving;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class WordRetriever {
+
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://randomword.com/").get();
+        var body = doc.body();
+        var word = body.getElementById("random_word").html();
+        System.out.println(word);
+    }
+}
