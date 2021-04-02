8699765a2d903c7ac3e0c992a8b03c9d55208914
Ethan Miller <ethan.romans5.8@gmail.com>
2021-04-02 11:26:08 -0500

    add background music and sound effects

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-unbound-preaching';
+  bgAudio = new Audio();
+  hoverSharp = new Audio();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.hoverSharp.src = "../assets/audio/hover__sharp.mp3";
+    this.hoverSharp.load();
+    this.playAudio();
+  }
+
+  playAudio(){
+    this.bgAudio.src = "../assets/audio/background.mp3";
+    this.bgAudio.load();
+    this.bgAudio.play();
+  }
+
+  toggleAudio(){
+    this.bgAudio.paused ? this.bgAudio.play() : this.bgAudio.pause();
+  }
+
+  playHoverSound() {
+    this.hoverSharp.play();
+  }
 }
