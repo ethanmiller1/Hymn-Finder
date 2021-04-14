c11b96e8f244cd87502d91168ce2ea25eebf62d1
Ethan Miller <ethan.romans5.8@gmail.com>
2021-04-13 22:19:31 -0500

    add videojs

---
+++ webapp/src/app/components/video-player/seek-plugin/seek-plugin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seek-plugin',
+  templateUrl: './seek-plugin.component.html',
+  styleUrls: ['./seek-plugin.component.scss']
+})
+export class SeekPluginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ webapp/src/app/components/video-player/video-player.component.ts
@@ -0,0 +1,52 @@
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+// import './seek-plugin';
+// import './notes-plugin';
+// import './sprites-plugin';
+import { videoJs } from './videojs';
+import {VideoJsOptions} from '../../model/videojs-options';
+
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss']
+})
+export class VideoPlayerComponent implements OnDestroy, AfterViewInit {
+  @ViewChild('target', { static: true })
+  target!: ElementRef;
+  @Input() options: VideoJsOptions = {};
+  player: any;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    // CustomVideoJsComponent.registerTitleComponent();
+    // CustomVideoJsComponent.registerCustomButton();
+    this.player = videoJs(this.target.nativeElement, this.options, this.onPlayerReady.bind(this));
+    this.player.customSeekButtons({
+      forward: 20,
+      backward: 15,
+      fwdBtnPosition: 1,
+      bwdBtnPosition: 1,
+      fwdBtnTooltip: `Seek 20 seconds forward`,
+      bwdBtnTooltip: `Seek 15 seconds backward`
+    });
+    this.player.notesButton({});
+    this.player.sprites({
+      spritesUrl: 'https://storage.googleapis.com/hubert-raymond-webpage/The_Hustler(1961)-sprites-256x144.png'
+    });
+  }
+
+  onPlayerReady() {
+    console.log('Player is ready.');
+    this.player.addChild('TitleBar', { text: 'Custom title bar component.' });
+    this.player.addChild('CustomButton');
+  }
+
+  ngOnDestroy(): void {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+}
